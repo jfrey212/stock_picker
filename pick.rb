29c13b127284b7db9137bd298b20f12d9f3d8282
jfrey212 <eojyerf@gmail.com>
2022-03-08 13:25:51 -0600

    Added some comments. Modified output to show profit with a string message

--- pick.rb
+++ pick.rb
@@ -8,7 +8,7 @@
 
 price1 = [17,3,6,9,15,8,6,1,10]
 price2 = [20, 9, 2, 22, 25, 18, 1, 10]
-price3 = [70, 65, 60, 50, 42, 39, 20, 15, 10, 15, 4, 1] # works for continuous loss, buy and sell on day 0 for no loss
+price3 = [70, 65, 60, 50, 42, 39, 20, 15, 10, 4, 1] # works for continuous loss, buy and sell on day 0 for no loss
 price4 = [4, 10, 20, 25, 22, 30, 37, 31, 48, 50, 45, 60] # works for continuous gain, buy first day, sell last day
 
 def pick(price_arr)
@@ -18,7 +18,7 @@ def pick(price_arr)
 
   price_arr.each_with_index do |price1, i|
     price_arr.each_with_index do |price2, j|
-      next unless j > i
+      next unless j > i                         # only compare current day with later days to keep sell date after buy date
       profit = price2 - price1
       if profit > largest_profit
         largest_profit = profit
@@ -27,7 +27,7 @@ def pick(price_arr)
       end
     end
   end
-  return [buy_day, sell_day, largest_profit]
+  return [buy_day, sell_day, "Profit = #{largest_profit}"]
 end
 
 p pick(price1)
